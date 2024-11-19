c66740da3530bdf3aefc6b18032229c09446e579
Kyle Ross <kyle@badger3000.com>
2024-11-19 13:54:03 -0700

    some styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   const title = "Choose your dog";
   const tagline = "We have the the largest selection of dogs";
   return (
-    <header className="flex flex-col m-auto text-center mb-8">
+    <header className="flex flex-col m-auto text-center mb-8 container px-2 lg:px-0 py-4">
       <Search />
       <h1 className="lg:text-5xl text-4xl font-bold lg:py-2 transition-all duration-200 mb-2 lg:mb-4">
         {title}
