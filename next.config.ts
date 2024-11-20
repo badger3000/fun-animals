920d66bdf8285ea2e39040204bd07bd68fe9fa02
Kyle Ross <kyle@badger3000.com>
2024-11-20 11:46:28 -0700

    updated config and changed icon

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,7 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.ctfassets.net",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    domains: ["images.ctfassets.net"],
   },
 };
 
